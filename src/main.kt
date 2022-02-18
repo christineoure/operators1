c8b34015ec64bec145f87bc2c9b532d05f6ac0dc
christineoure <ourechristine@gmail.com>
2022-02-18 21:52:01 +0300

    add my file

---
+++ src/main.kt
@@ -0,0 +1,22 @@
+fun main(){
+    greet("Santana")
+   quotient(25,4)
+    add(5,8,13,28)
+    myfact()
+}
+fun greet(name:String){
+    var name = "Santana"
+    println("Hello "+name)
+}
+fun quotient (a:Int,b:Int){
+    var reminder = a/b
+    println(reminder)
+}
+fun add (a:Int,b:Int,c:Int,d:Int){
+    var sum = a+b+c+d
+    println(sum)
+}
+
+fun myfact(){
+    println("I am a dancer")
+}
